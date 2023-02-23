fcb1f67f481a303a3109177b13a0f1677252f97c
Matthew <99067338@mydavinci.nl>
2023-02-23 10:32:34 +0100

    array met namen klaar

--- array met namen/array met namen.js
+++ array met namen/array met namen.js
@@ -1,4 +1,9 @@
-const namen = [];
-
-document.getElementById('namen').innerHTML = 'Alle dagen van de week zijn: '+ namen
-document.getElementById('namenreversed').innerHTML = "De werkdagen zijn:" + namen.reverse()
+var count = 0
+namen = []
+count = prompt('Hoeveel namen wilt u in de array stoppen?')
+for (var i = 0; i <= count;i++){
+    ingevuldenamen = prompt('Vul hier de namen 1 voor 1 in:')
+    namen.push(ingevuldenamen)
+}
+document.getElementById('namen').innerHTML = 'Alle namen: '+ namen
+document.getElementById('namenreversed').innerHTML = "De namen reversed zijn:" + namen.reverse()
