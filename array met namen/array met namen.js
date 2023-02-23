4a938dec7def8a41a0d0b983e1645a43c6a0f5dd
Matthew <99067338@mydavinci.nl>
2023-02-23 10:11:33 +0100

    array met dagen files start aangemaakt

---
+++ array met namen/array met namen.js
@@ -0,0 +1,4 @@
+const namen = [];
+
+document.getElementById('namen').innerHTML = 'Alle dagen van de week zijn: '+ namen
+document.getElementById('namenreversed').innerHTML = "De werkdagen zijn:" + namen.reverse()
