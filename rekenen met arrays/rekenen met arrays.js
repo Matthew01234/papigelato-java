fef60865ac06289901bf8e36177cd71327ebcc7d
Matthew <99067338@mydavinci.nl>
2023-03-06 15:28:47 +0100

    reken met array klaar

--- rekenen met arrays/rekenen met arrays.js
+++ rekenen met arrays/rekenen met arrays.js
@@ -1,10 +1,23 @@
 var counter = 0
 var arrayEen = [1,2,3,4,5,6,7,8,9,10];
 var arrayTwee = [2,4,6,8,10,12,14,16,18,20];
-for (var count = 1; counter <= 2;counter++){
-    h1 = arrayEen + arrayTwee
-    h2 = arrayEen - arrayTwee
-}
+var h1 = [];
+var h2 = [];
+var h3 = [];
+var h4 = [];
+
+
+for(var i = 0;i<=arrayTwee.length-1;i++)
+  h1.push(arrayEen[i] - arrayTwee[i]);
+
+for(var i = 0;i<=arrayTwee.length-1;i++)
+    h2.push(arrayEen[i] + arrayTwee[i]);
+
+for(var i = 0;i<=arrayTwee.length-1;i++)
+    h3.push(arrayEen[i] / arrayTwee[i]);
+
+for(var i = 0;i<=arrayTwee.length-1;i++)
+    h4.push(arrayEen[i] * arrayTwee[i]);
 
 document.getElementById('h1').innerHTML = h1
 document.getElementById('h2').innerHTML = h2
