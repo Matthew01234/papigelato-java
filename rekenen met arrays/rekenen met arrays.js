d7daee9f8d0e1e6482cf5ebe77bb75324393c3f3
Matthew <99067338@mydavinci.nl>
2023-03-06 13:48:07 +0100

    new layout voor alles

---
+++ rekenen met arrays/rekenen met arrays.js
@@ -0,0 +1,12 @@
+var counter = 0
+var arrayEen = [1,2,3,4,5,6,7,8,9,10];
+var arrayTwee = [2,4,6,8,10,12,14,16,18,20];
+for (var count = 1; counter <= 2;counter++){
+    h1 = arrayEen + arrayTwee
+    h2 = arrayEen - arrayTwee
+}
+
+document.getElementById('h1').innerHTML = h1
+document.getElementById('h2').innerHTML = h2
+document.getElementById('h3').innerHTML = h3
+document.getElementById('h4').innerHTML = h4
