5c32124c7f8725f8e2e708d6417aa61b2848b9e2
Matthew <99067338@mydavinci.nl>
2023-03-07 11:30:44 +0100

    array met tagels af

--- array met tafels/array met tafels.js
+++ array met tafels/array met tafels.js
@@ -3,6 +3,7 @@ var h2 = []
 for (var count = 0; count < tafels.length;count++){
     for (var count2 = 1; count2 <= 10;count2++){
         h2.push(tafels[count] * count2);
+        
     }
 }
 document.getElementById('h2').innerHTML = h2
